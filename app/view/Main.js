26c6b6d7c561ffe68a18b290830d839092d0cf5b
Diego García <chinopb@gmail.com>
2014-08-17 12:27:31 -0600

    Code

---
+++ app/view/Main.js
@@ -0,0 +1,29 @@
+Ext.define('FakebookConnect.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Welcome',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Sencha Touch 2'
+                },
+
+                html: 'This is a facebook connection test'
+            }
+        ]
+    }
+});
